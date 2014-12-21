4230253afe7d09370c706a2ec4793007e716b93a
Goose <goosemixes@gmail.com>
2014-12-21 12:25:54 -0500

    Header and Footer Detailing
    Added all the typical needed information. Styles, Scripts, Favicon, and
    more set up

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,3 +1,9 @@
-<strong>&copy; 2015</strong>
+<footer>
+<!-- Copyright -->
+	<div id="copyright"><strong> &copy; 2015 </strong></div>
+</footer>
+
+<!-- JavaScript -->
+<script type="text/javascript" src="script.js"></script>
 </body>
 </html>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,6 +1,33 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-	<title><?php echo $title ?> - CodeIgniter 2 Tutorial</title>
+<!-- For character display -->
+	<meta charset="utf-8">
+<!-- Title -->
+	<title><?php echo $title ?></title>
+<!-- For mobile views -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- For Crawlers -->
+	<meta name="keywords" content="">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<meta name="robots" CONTENT="all">
+<!-- Fallback for Internet Explorer 9 -->
+	<!--[if lt IE 9]>
+	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+<!-- Favicon Icon -->
+    <link rel="shortcut icon" href="favicon.ico">
+	<!-- <link rel="icon" href="resources/favicon.ico" type="image/x-icon" /> -->
+<!-- CSS -->
+	<link type="text/css" rel="stylesheet" href="resources/style.css">
 </head>
 <body>
+
+<header>
 	<h1>Health Web App</h1>
+	<div id="nav-bar">
+		<a id="home" class="nav-item" type="button" href="/health/">Home</a>
+		<a id="about" class="nav-item" type="button" href="/health/about">About</a>
+	</div>
+</header>
