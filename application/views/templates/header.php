e35bdba2adeefd7ae19219728137dc751f972dea
Goose <goosemixes@gmail.com>
2015-02-03 15:42:34 -0500

    Login moved to header, join moved to welcome page
    Notice that the links to the login page and join page are disabled, but
    when disabled, can still be visited.

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -63,8 +63,6 @@
 <header>
 	
 	<div class="wrapper">
-		
-		<!-- <h1>Health Web App</h1> -->
 	
 	<!-- show profile picture if user is logged in -->
 			<?php if ( isset($log_check) ) { ?>
@@ -84,7 +82,7 @@
 			<div class="fr">
 				
 	<!-- Dropdown -->
-	<!--
+	
 			<div class="select-cnt">
 				<select class="nav-select" onChange="window.location.href=this.value">
 				  <option><?php echo $name; ?></option>
@@ -95,14 +93,15 @@
 				  <option value="<?=base_url()?>login/logout">Logout</a></option>
 				</select>
 			</div>
-	<!-- Profile Image --><!--
+	<!-- Profile Image -->
+		<!--
 			<div class="profile-img-cnt">
 				<a href="<?=base_url()?>users/<?php echo $self['username']; ?>">
-				<img class="nav-img" src="<?=base_url()?>uploads/<?php echo $self['image']; ?>"/>
-	
+					<img class="nav-img" src="<?=base_url()?>uploads/<?php echo $self['image']; ?>"/>
 				</a>
 			</div>
-			<?php } ?></div><!-->
+			<?php } ?></div>
+		-->
 		
 	</div><!--wrapper-->
 	
@@ -143,8 +142,85 @@
 <!-- Unlogged in navbar -->
 		<a href="<?=base_url()?>">Home</a> |
 		<a href="<?=base_url()?>about">About</a> |
-		<a href="<?=base_url()?>dashboard">Login</a> |
-		<a href="<?=base_url()?>join">Join</a> |
+		<!-- <a href="<?=base_url()?>dashboard">Login</a> | -->
+		<!-- <a href="<?=base_url()?>join">Join</a> | -->
+
+		  <?php echo validation_errors(); ?>
+		  <?php echo form_open('login/verifylogin'); ?>
+		   <label for="username">Username:</label>
+		   <input type="text" name="username"/>
+		   <label for="password">Password:</label>
+		   <input type="password" name="password"/>
+		   <input type="submit" value="Login"/>
+		  </form>
+
+		<script>
+		  // This is called with the results from from FB.getLoginStatus().
+		  function statusChangeCallback(response) {
+		    console.log('statusChangeCallback');
+		    console.log(response);
+		    // The response object is returned with a status field that lets the
+		    // app know the current login status of the person.
+		    if (response.status === 'connected') {
+		      // Logged into your app and Facebook.
+		      testAPI();
+		    } else if (response.status === 'not_authorized') {
+		      // The person is logged into Facebook, but not your app.
+		      document.getElementById('status').innerHTML = 'Please log ' +
+		        'into this app.';
+		    } else {
+		      // The person is not logged into Facebook, so we're not sure if
+		      // they are logged into this app or not.
+		      document.getElementById('status').innerHTML = 'Please log ' +
+		        'into Facebook.';
+		    }
+		  }
+		  // This function is called when someone finishes with the Login Button.
+		  function checkLoginState() {
+		    FB.getLoginStatus(function(response) {
+		      statusChangeCallback(response);
+		    });
+		  }
+		  window.fbAsyncInit = function() {
+		  FB.init({
+		    appId      : '691232867658713',
+		    cookie     : true,  // enable cookies to allow the server to access 
+		                        // the session
+		    xfbml      : true,  // parse social plugins on this page
+		    version    : 'v2.1' // use version 2.1
+		  });
+		  // FB.getLoginStatus().  This function gets the state of the
+		  // person visiting this page and can return one of three states to
+		  // the callback you provide.  They can be:
+		  // 1. Logged into your app ('connected')
+		  // 2. Logged into Facebook, but not your app ('not_authorized')
+		  // 3. Not logged into Facebook and can't tell if they are logged into
+		  //    your app or not.
+		  // These three cases are handled in the callback function.
+		  FB.getLoginStatus(function(response) {
+		    statusChangeCallback(response);
+		  });
+		  };
+		  // Here we run a very simple test of the Graph API after login is
+		  // successful.  See statusChangeCallback() for when this call is made.
+		  function testAPI() {
+		    console.log('Welcome!  Fetching your information.... ');
+		    FB.api('/me', function(response) {
+		      console.log('Successful login for: ' + response.name);
+		      document.getElementById('status').innerHTML =
+		        'Thanks for logging in, ' + response.name + '!';
+		      var username = response.name;
+		      var email = response.email;
+		      var id = response.id;
+		      window.location.replace("http://interplay.xyz/health/facebook_login/"+username+"/"+email+"/"+id+"");
+		    });
+		  }
+		</script>
+		<!-- Login Button -->
+		<fb:login-button scope="public_profile,email" onlogin="checkLoginState();">
+		</fb:login-button>
+		<div id="status"></div>
+
 		<?php } ?>
 
 		</div>
