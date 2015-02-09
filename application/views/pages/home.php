0612879491cf6815dd392c40b9c72e2937aef295
surrealpixel <john@surrealpixel.com>
2015-02-09 17:29:01 -0500

    Update

---
+++ application/views/pages/home.php
@@ -0,0 +1,7 @@
+<div class="wrapper">
+	
+	<div class="content">
+		
+	</div><!--content-->
+	
+</div><!--wrapper-->
--- application/views/user/password_form.php
+++ application/views/user/password_form.php
@@ -1,19 +1,26 @@
-     <h3>Change your password</h3> 
-   <?php echo validation_errors(); ?>
-   <?php echo form_open('dashboard/set_password'); ?>
+<div class="mt-med">
+	
+	<div class="content-full">
+    
+    	<h1>Modify Password</h1>
+		<p>We encourage you to choose a strong password for maximum security</p>
+		
+		<?php echo validation_errors(); ?>
+		<?php echo form_open('dashboard/set_password'); ?>
 
-     <label for="existing_password">Existing Password:</label>
-     <input class="input-textarea"
-     type="password" size="20" id="existing_password" name="existing_password"/>
-     <br/>
-     <label for="password">New Password:</label>
-     <input class="input-textarea"
-     type="password" size="20" id="password" name="password"/>
-     <br/>
-     <label for="confirm_password">Confirm Password:</label>
-     <input class="input-textarea"
-     type="password" size="20" id="confirm_password" name="confirm_password"/>
-     <br/>
-     <input class="input-textarea"
-     type="submit" value="Submit Changes"/>
-   </form>
+			<label for="existing_password">Existing Password:</label>
+			<input class="input-textarea" type="password" size="20" id="existing_password" name="existing_password"/>
+		
+			<label for="password">New Password:</label>
+			<input class="input-textarea" type="password" size="20" id="password" name="password"/>
+   
+			<label for="confirm_password">Confirm Password:</label>
+			<input class="input-textarea" type="password" size="20" id="confirm_password" name="confirm_password"/>
+     
+			<input class="input-textarea" type="submit" value="Submit Changes"/>
+		
+		</form>
+   
+   </div><!--content-full-->
+   
+</div><!--mt-med-->
