eb3a3731f6b3e257f58ef4306f05d88cb5d47e79
surrealpixel <john@surrealpixel.com>
2015-02-09 00:13:01 -0500

    Changes
    Check Trello for some issues

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,11 +1,5 @@
 <footer>
-<hr/>
-<!-- Copyright -->
-	<div id="copyright"><strong> &copy; 2015 </strong></div>
-</footer>
 
-<!-- End page wrap -->
-</div>
 
 <!-- JavaScript -->
 <script type="text/javascript" src="<?=base_url()?>resources/tools/jquery-1.8.3.min.js"></script>
