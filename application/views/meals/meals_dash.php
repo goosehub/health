48b5d4c0b01369a4e9dcb483043237c5a1bbac17
surrealpixel <john@surrealpixel.com>
2015-02-13 19:54:18 -0500

    Changes now

--- application/views/meals/meals_dash.php
+++ application/views/meals/meals_dash.php
@@ -1,4 +1,30 @@
-<h1>Meal Tracker</h1
-<hr/>
-<h2><a href="meals/new">Enter a New Meal</a></h2>
-<h2><a href="../users/<?php echo $self['username']; ?>/meals">See your Meals History</a></h2>
+<div class="mt-med">
+	
+	<div class="content-full">
+		
+		<div class="content">
+			
+			<div class="fl"><div class="mb-med"><div class="nest">
+			
+			<h1>Nutrition Tracker</h1>
+			<div class="mb-med"><p>Track your nutrition to ensure that you are on the right path.</p></div>
+			
+			</div></div></div>
+			
+			<div class="fr"><div class="nest"><a class="cta" href="#">Add a Meal</a></div></div>
+			
+		
+		</div><!--content-->
+		
+		<span class="divider">
+		
+		<div class="content">
+		
+			<h2><a href="meals/new">Enter a New Meal</a></h2>
+			<h2><a href="../users/<?php echo $self['username']; ?>/meals">See your Meals History</a></h2>
+		
+		</div><!--content-->
+		
+	</div><!--content-full-->
+	
+</div><!--mt-med-->
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -52,11 +52,7 @@
 					</form>
 					
 			</div><!--fl-->
-<<<<<<< Updated upstream
 
-=======
-					
->>>>>>> Stashed changes
 				<!-- Dropdown -->
 				<div class="fr">
 					
@@ -93,19 +89,17 @@
 					</div><!--nest-->
 					
 				</div><!--fr-->
-<<<<<<< Updated upstream
-=======
+
 				
 			</div><!--fr-->
->>>>>>> Stashed changes
+
 
 			</div><!--wrapper-->
 
 			<!-- User is NOT logged in -->
-<<<<<<< Updated upstream
+
 				<?php } ?>
-=======
->>>>>>> Stashed changes
+
 				
 				<!-- User is NOT logged in -->
 				<div class="wrapper">
