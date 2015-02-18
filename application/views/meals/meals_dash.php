de93f63cc0de0d5f0ce1d66745ce81980ea20700
surrealpixel <john@surrealpixel.com>
2015-02-17 22:40:08 -0500

    Meals Dashboard v1
    I have completed the first version of the Meals Dashboard. Let me know
    if you have any questions about it. Also removed “newstyle.css” and put
    all styles in “styles.css” — over the next day or so I’ll be going over
    all of the HTML/CSS and organizing it.

--- application/views/meals/meals_dash.php
+++ application/views/meals/meals_dash.php
@@ -1,30 +1,125 @@
 <div class="mt-med">
 	
-	<div class="content-full">
+	<div class="content">
 		
-		<div class="content">
+		<div class="inline-b">
+		
+			<div class="nest">
 			
-			<div class="fl"><div class="mb-med"><div class="nest">
+				<div class="p-med">
+					
+					<div class="fl">
+						
+						<div class="mb-med">
+								
+								<h1>Nutrition Log (<a href="#" class="link-01">March 12</a>)</h1>
+								<p>Keep track of your daily nutritional intake.</p>
+							
+						</div><!--mb-med-->
+						
+					</div><!--fl-->
+					
+					<div class="fr">
+							
+							<div class="ml-med">
+							
+								<div class="list-01">
+								
+									<ul>
+									
+										<li>1,286 of 3,240 Calories</li>
+										<li>54g of 80g Protein</li>
+										<li>28g of 60g Fat</li>
+										<li>26g of 26g Carbohydrates</li>
+									
+									</ul>
+									
+								</div><!--list-01-->
+								
+							</div><!--ml-med-->
+						
+					</div><!--fr-->
+					
+				</div><!--p-med-->
+				
+			</div><!--nest-->
+		
+		</div><!--inline-b-->
+		
+		<div class="nest">
 			
-			<h1>Nutrition Tracker</h1>
-			<div class="mb-med"><p>Track your nutrition to ensure that you are on the right path.</p></div>
+			<table>
+				
+				<tr>
+					
+					<th>Food Name</th>
+					<th>Calories</th>
+					<th>Protein</th>
+					<th>Fat</th>
+					<th>Carbohydrates</th>
+					<th></th>
+					
+				</tr>
+				
+				<tr>
+					
+					<td><input class="ftext" placeholder="e.g. Fruity Pebbles"></input></td>
+					<td><input class="fnum" placeholder="Calories"></input></td>
+					<td><input class="fnum" placeholder="Protein"></input></td>
+					<td><input class="fnum" placeholder="Fat"></input></td>
+					<td><input class="fnum" placeholder="Carbs"></input></td>
+					<td><a href="#" class="bl-btn-sml">Add a Meal</a></td>
+					
+				</tr>
+				
+				<tr>
+					
+					<td>Chicken Tenders</td>
+					<td>300</td>
+					<td>40</td>
+					<td>36</td>
+					<td>84</td>
+					<td><a href="#" class="hlink"><span class="icon-close"></span></a></td>
+					
+				</tr>
+				
+				<tr>
+					
+					<td>Whole Milk (2 cups)</td>
+					<td>300</td>
+					<td>16</td>
+					<td>12</td>
+					<td>14</td>
+					<td><a href="#" class="hlink"><span class="icon-close"></span></a></td>
+					
+				</tr>
+				
+				<tr>
+					
+					<td>Cheeseburger</td>
+					<td>642</td>
+					<td>48</td>
+					<td>36</td>
+					<td>84</td>
+					<td><a href="#" class="hlink"><span class="icon-close"></span></a></td>
+					
+				</tr>
+				
+				<tr class="total-consumed">
+					
+					<td>Total Consumed</td>
+					<td>1,286</td>
+					<td>54</td>
+					<td>28</td>
+					<td>26</td>
+					<td></td>
+					
+				</tr>
+				
+			</table>
 			
-			</div></div></div>
-			
-			<div class="fr"><div class="nest"><a class="cta" href="#">Add a Meal</a></div></div>
-			
+		</div><!--nest-->
 		
-		</div><!--content-->
-		
-		<span class="divider">
-		
-		<div class="content">
-		
-			<h2><a href="meals/new">Enter a New Meal</a></h2>
-			<h2><a href="../users/<?php echo $self['username']; ?>/meals">See your Meals History</a></h2>
-		
-		</div><!--content-->
-		
-	</div><!--content-full-->
+	</div><!--content-->
 	
 </div><!--mt-med-->
